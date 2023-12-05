2bd78a3d4d0cb0856395bb56c5cc0649fabec5ae
Darrell <darrell.anderson@gmail.com>
2023-12-05 15:59:18 -0800

    leave seat moves to open slot (not specator)

--- src/global/content-menu/leave-seat.ts
+++ src/global/content-menu/leave-seat.ts
@@ -1,16 +1,40 @@
 import { Player, globalEvents, world } from "@tabletop-playground/api";
 import { AbstractGlobal } from "../abstract-global";
 
+/**
+ * Content menu item to leave seat.  Move to an unused slot, NOT the
+ * spectator slot.  Spectators cannot interact, preventing them from
+ * clicking any "take seat" buttons.
+ */
 export class LeaveSeat implements AbstractGlobal {
-  init(): void {
+  public init(): void {
     const actionName: string = "*Leave seat";
-    const tooltip: string = "Switch to spectator";
+    const tooltip: string = "Switch to non-seat player slot";
     world.addCustomAction(actionName, tooltip);
 
     globalEvents.onCustomAction.add((player: Player, identifier: string) => {
       if (identifier === actionName) {
-        player.switchSlot(-1);
+        LeaveSeat.leaveSeat(player);
       }
     });
+  }
+
+  static leaveSeat(clickingPlayer: Player) {
+    const busy = new Set();
+    for (const player of world.getAllPlayers()) {
+      busy.add(player.getSlot());
+    }
+    for (const obj of world.getAllObjects()) {
+      busy.add(obj.getOwningPlayerSlot());
+    }
+    for (let i = 0; i < 20; i++) {
+      if (!busy.has(i)) {
+        console.log(
+          `LeaveSeat: moving "${clickingPlayer.getName()}" to open slot ${i}`
+        );
+        clickingPlayer.switchSlot(i);
+        break;
+      }
+    }
   }
 }
