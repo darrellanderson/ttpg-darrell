d95821fabc5c27440fbc33a663eb736239c72d1d
Darrell <darrell.anderson@gmail.com>
2024-01-30 21:17:46 -0800

    typo

--- src/lib/svg/svg-sparkline.test.ts
+++ src/lib/svg/svg-sparkline.test.ts
@@ -10,7 +10,7 @@ it("sparkline", () => {
 it("sparkline (empty)", () => {
     const svg: string = SvgSparkline.svg([]);
     expect(svg).toEqual(
-        "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 100'>/svg>"
+        "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 100'></svg>"
     );
 });
 
