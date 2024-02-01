034dcafeb7c5ad6f249119cad442db9bd3452e9a
Darrell <darrell.anderson@gmail.com>
2024-02-01 00:59:07 -0800

    start adjacency

---
+++ src/lib/adjacency/adjacency.ts
@@ -0,0 +1,16 @@
+export class Adjacency {
+    addLink(type: string, a: string, b: string): this {
+        // TODO XXX
+        return this;
+    }
+
+    blockLink(type: string, a: string, b: string): this {
+        // TODO XXX
+        return this;
+    }
+
+    getAdjacent(distance: number): string[] {
+        // TODO XXX
+        return [];
+    }
+}
