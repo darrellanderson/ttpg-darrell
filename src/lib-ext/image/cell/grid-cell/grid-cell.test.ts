1f36e8e35030a4e0a1dad5576406947a5d3e7286
darrell <darrell.anderson@gmail.com>
2024-02-10 23:52:15 -0800

    grid

--- src/lib-ext/image/cell/grid-cell/grid-cell.test.ts
+++ src/lib-ext/image/cell/grid-cell/grid-cell.test.ts
@@ -1,5 +1,12 @@
+import { CellSize } from "../abstract-cell/abstract-cell";
 import { GridCell } from "./grid-cell";
 
 it("constructor", () => {
-    new GridCell(1, 1);
+    new GridCell([]);
+});
+
+it("getMaxCells", () => {
+    const cellSize: CellSize = { width: 500, height: 750 };
+    const max = GridCell.getMaxCells(cellSize);
+    expect(max).toEqual(40);
 });
--- src/lib-ext/image/cell/grid-cell/grid-cell.ts
+++ src/lib-ext/image/cell/grid-cell/grid-cell.ts
@@ -2,11 +2,17 @@ import { AbstractCell, CellSize } from "../abstract-cell/abstract-cell";
 
 export class GridCell extends AbstractCell {
     static readonly MAX_DIMENSION = 4096;
+
     static getMaxCells(cellSize: CellSize): number {
         const { width, height } = cellSize;
         const maxCols = Math.floor(this.MAX_DIMENSION / width);
         const maxRows = Math.floor(this.MAX_DIMENSION / height);
         return maxCols * maxRows;
+    }
+
+    constructor(cells: Array<AbstractCell>) {
+        const size = cells[0]?.getSize() ?? { width: 0, height: 0 };
+        super(size.width, size.height);
     }
 
     public toBuffer(): Promise<Buffer> {
