b6817dbcff585c2fe1815c020c047a19e7e823d4
darrell <darrell.anderson@gmail.com>
2024-02-13 23:16:22 -0800

    template

---
+++ src/lib-ext/template/cube-template/cube-template.data.ts
@@ -0,0 +1,89 @@
+export const CUBE_SUB_TEMPLATE = {
+    Model: "$MODEL_HERE",
+    Offset: {
+        X: 0,
+        Y: 0,
+        Z: 0,
+    },
+    Scale: {
+        X: 1,
+        Y: 1,
+        Z: 1,
+    },
+    Rotation: {
+        X: 0,
+        Y: 0,
+        Z: 0,
+    },
+    Texture: "$TEXTURE HERE",
+    NormalMap: "",
+    ExtraMap: "",
+    ExtraMap2: "",
+    IsTransparent: false,
+    CastShadow: true,
+    IsTwoSided: false,
+    UseOverrides: true,
+    SurfaceType: "Cardboard",
+};
+
+export const CUBE_TEMPLATE = {
+    Type: "Generic",
+    GUID: "$GUID HERE",
+    Name: "$NAME HERE",
+    Metadata: "",
+    CollisionType: "Regular",
+    Friction: 0.7,
+    Restitution: 0.3,
+    Density: 1,
+    SurfaceType: "Cardboard",
+
+    Roughness: 1,
+    Metallic: 0,
+    PrimaryColor: {
+        R: 255,
+        G: 255,
+        B: 255,
+    },
+    SecondaryColor: {
+        R: 0,
+        G: 0,
+        B: 0,
+    },
+    Flippable: false,
+    AutoStraighten: false,
+    ShouldSnap: false,
+    ScriptName: "",
+    Blueprint: "",
+    Models: ["$REPLACE THIS"],
+    Collision: [
+        {
+            Model: "utility/unit-cube.obj",
+            Offset: {
+                X: 0,
+                Y: 0,
+                Z: 0,
+            },
+            Scale: {
+                X: 1,
+                Y: 1,
+                Z: 1,
+            },
+            Rotation: {
+                X: 0,
+                Y: 0,
+                Z: 0,
+            },
+            Type: "Convex",
+        },
+    ],
+    Lights: [],
+    SnapPointsGlobal: false,
+    SnapPoints: [{}],
+    ZoomViewDirection: {
+        X: 0,
+        Y: 0,
+        Z: 1,
+    },
+    GroundAccessibility: "Zoom",
+    Tags: [],
+};
---
+++ src/lib-ext/template/cube-template/cube-template.test.ts
@@ -0,0 +1,78 @@
+import { CubeTemplate, CubeTemplateBoundingBox } from "./cube-template";
+
+it("static getBoundingBox (empty)", () => {
+    const bb: CubeTemplateBoundingBox = CubeTemplate.getBoundingBox([
+        {
+            texture: "my-texture",
+            model: "my-model",
+            width: 2,
+            height: 2,
+            depth: 2,
+        },
+        {
+            texture: "my-texture",
+            model: "my-model",
+            width: 3,
+            height: 1,
+            depth: 1,
+            left: 2,
+            top: 0,
+        },
+    ]);
+    expect(bb).toEqual({ bottom: 2, left: 0, maxDepth: 2, right: 5, top: 0 });
+});
+
+it("static getBoundingBox (empty)", () => {
+    const bb: CubeTemplateBoundingBox = CubeTemplate.getBoundingBox([]);
+    expect(bb).toEqual({ bottom: 0, left: 0, maxDepth: 0, right: 0, top: 0 });
+});
+
+it("constructor", () => {
+    new CubeTemplate();
+});
+
+it("add/set", () => {
+    new CubeTemplate()
+        .addEntry({
+            texture: "my-texture",
+            model: "my-model",
+            width: 1,
+            height: 1,
+            depth: 1,
+        })
+        .setGuidFrom("path/to/template.json")
+        .setName("my-name");
+});
+
+it("toTemplate (no entries)", () => {
+    expect(() => {
+        new CubeTemplate().setGuidFrom("foo").toTemplate();
+    }).toThrow();
+});
+
+it("toTemplate (no guid)", () => {
+    expect(() => {
+        new CubeTemplate()
+            .addEntry({
+                texture: "my-texture",
+                model: "my-model",
+                width: 1,
+                height: 1,
+                depth: 1,
+            })
+            .toTemplate();
+    }).toThrow();
+});
+
+it("toTemplate", () => {
+    new CubeTemplate()
+        .addEntry({
+            texture: "my-texture",
+            model: "my-model",
+            width: 1,
+            height: 1,
+            depth: 1,
+        })
+        .setGuidFrom("foo")
+        .toTemplate();
+});
