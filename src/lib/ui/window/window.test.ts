77df4c6eaa696ef75bfb812f18c43679a088539b
darrell <darrell.anderson@gmail.com>
2024-02-20 21:46:06 -0800

    test coverage

--- src/lib/ui/window/window.test.ts
+++ src/lib/ui/window/window.test.ts
@@ -3,12 +3,25 @@ import { Window } from "./window";
 import { WindowParams } from "./window-params";
 
 it("constructor", () => {
-    new Window({
+    const params: WindowParams = {
         size: { width: 1, height: 1 },
         createWidget: (scale: number): Widget => {
             return new LayoutBox()
                 .setOverrideHeight(1 * scale)
                 .setOverrideWidth(1 * scale);
         },
-    });
+    };
+    new Window(params, [1, 2, 3]);
+});
+
+it("attach/detach", () => {
+    const params: WindowParams = {
+        size: { width: 1, height: 1 },
+        createWidget: (scale: number): Widget => {
+            return new LayoutBox()
+                .setOverrideHeight(1 * scale)
+                .setOverrideWidth(1 * scale);
+        },
+    };
+    new Window(params, [1, 2, 3]).attach().detach();
 });
--- src/lib/ui/window/window.ts
+++ src/lib/ui/window/window.ts
@@ -1,3 +1,4 @@
+import { PlayerWindow } from "./player-window";
 import { WindowParams } from "./window-params";
 
 /**
@@ -5,19 +6,25 @@ import { WindowParams } from "./window-params";
  * (starts in world for VR players).  Optionally allow collapse, close.
  */
 export class Window {
-    private readonly _params: WindowParams;
+    private readonly _playerWindows: Array<PlayerWindow>;
 
-    constructor(params: WindowParams) {
-        this._params = params;
+    constructor(params: WindowParams, playerSlots: Array<number>) {
+        this._playerWindows = playerSlots.map(
+            (playerSlot) => new PlayerWindow(params, playerSlot)
+        );
     }
 
     attach(): this {
-        // TODO XXX
+        for (const playerWindow of this._playerWindows) {
+            playerWindow.attach();
+        }
         return this;
     }
 
     detach(): this {
-        // TODO XXX
+        for (const playerWindow of this._playerWindows) {
+            playerWindow.detach();
+        }
         return this;
     }
 }
