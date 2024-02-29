4abbc536d17cd366af23e9e63b250046b6fc9dbe
darrell <darrell.anderson@gmail.com>
2024-02-28 18:33:17 -0800

    weighted choice guts, tests

--- src/lib/weighted-choice/weighted-choice.test.ts
+++ src/lib/weighted-choice/weighted-choice.test.ts
@@ -1,5 +1,38 @@
 import { WeightedChoice } from "./weighted-choice";
 
 it("constructor", () => {
-    new WeightedChoice();
+    new WeightedChoice([]);
+});
+
+it("constructor (negative weight)", () => {
+    expect(() => {
+        new WeightedChoice<string>([
+            {
+                value: "a",
+                weight: -1,
+            },
+        ]);
+    }).toThrow();
+});
+
+it("choice", () => {
+    let randomCalls = 0;
+    jest.spyOn(Math, "random").mockImplementation((): number => {
+        return (randomCalls++ % 10) / 10;
+    });
+
+    const weightedChoice = new WeightedChoice<string>([
+        { value: "a", weight: 1 },
+        { value: "b", weight: 3 },
+    ]);
+
+    const choiceCount: { [key: string]: number } = { a: 0, b: 0 };
+    for (let i = 0; i < 100; i++) {
+        const choice: string = weightedChoice.choice();
+        choiceCount[choice]++;
+    }
+    expect(Math.abs(choiceCount["a"] ?? 0) - 25).toBeLessThan(10);
+    expect(Math.abs(choiceCount["b"] ?? 0) - 75).toBeLessThan(10);
+
+    jest.restoreAllMocks();
 });
--- src/lib/weighted-choice/weighted-choice.ts
+++ src/lib/weighted-choice/weighted-choice.ts
@@ -1,1 +1,35 @@
-export class WeightedChoice {}
+export type WeightedChoiceOption<T> = {
+    weight: number;
+    value: T;
+};
+
+export class WeightedChoice<T> {
+    private readonly _options: Array<WeightedChoiceOption<T>>;
+    private readonly _totalWeight: number;
+
+    constructor(options: Array<WeightedChoiceOption<T>>) {
+        options.forEach((option) => {
+            if (option.weight < 0) {
+                throw new Error("weight must be non-negative");
+            }
+        });
+
+        this._options = options;
+        this._totalWeight = this._options
+            .map((option): number => option.weight)
+            .reduce((prevValue: number, currentValue: number): number => {
+                return prevValue + currentValue;
+            }, 0);
+    }
+
+    choice(): T {
+        let target = Math.random() * this._totalWeight;
+        for (const option of this._options) {
+            if (target <= option.weight) {
+                return option.value;
+            }
+            target -= option.weight;
+        }
+        throw new Error("unreachable");
+    }
+}
