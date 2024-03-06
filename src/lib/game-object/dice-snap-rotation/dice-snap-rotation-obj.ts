cee6a62286ca26f9c5e19661c68ee3762024da6e
darrell <darrell.anderson@gmail.com>
2024-03-05 23:46:16 -0800

    remove dice snap rotation

--- src/lib/game-object/dice-snap-rotation/dice-snap-rotation-obj.ts
+++
@@ -1,4 +0,0 @@
-import { refDice } from "@tabletop-playground/api";
-import { DiceSnapRotation } from "./dice-snap-rotation";
-
-new DiceSnapRotation(refDice);
--- src/lib/game-object/dice-snap-rotation/dice-snap-rotation.test.ts
+++
@@ -1,8 +0,0 @@
-import { MockDice } from "ttpg-mock";
-import { DiceSnapRotation } from "./dice-snap-rotation";
-import { Dice } from "@tabletop-playground/api";
-
-it("constructor", () => {
-    const dice: Dice = new MockDice();
-    new DiceSnapRotation(dice);
-});
