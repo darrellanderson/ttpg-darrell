0cb7738b4df3bb85f80a21787c0e4a40a74bc25d
Darrell <darrell.anderson@gmail.com>
2024-07-31 14:30:41 -0700

    typos

--- src/lib/bug-workarounds/bug-force-transform-updates/bug-force-transform-updates.test.ts
+++ src/lib/bug-workarounds/bug-force-transform-updates/bug-force-transform-updates.test.ts
@@ -1,30 +1,38 @@
 import { MockGameObject } from "ttpg-mock";
-import { BugForceTransformUpdates, DELTA } from "./bug-force-transform-updates"; // replace with your class's path
+import { BugForceTransformUpdates, DELTA } from "./bug-force-transform-updates";
 
 it("poke", () => {
     const bugForceTransformUpdatesMock = new BugForceTransformUpdates();
     const obj = new MockGameObject();
     const x = obj.getPosition().x;
+    const yaw = obj.getRotation().yaw;
 
     bugForceTransformUpdatesMock._maybeStartPoking(obj);
 
     bugForceTransformUpdatesMock.pokeAll();
     expect(obj.getPosition().x).toBe(x + DELTA);
+    expect(obj.getRotation().yaw).toBe(yaw + DELTA);
 
     bugForceTransformUpdatesMock.pokeAll();
     expect(obj.getPosition().x).toBe(x);
+    expect(obj.getRotation().yaw).toBe(yaw);
 
     bugForceTransformUpdatesMock.pokeAll();
     expect(obj.getPosition().x).toBe(x + DELTA);
+    expect(obj.getRotation().yaw).toBe(yaw + DELTA);
 
     bugForceTransformUpdatesMock.pokeAll();
     expect(obj.getPosition().x).toBe(x);
+    expect(obj.getRotation().yaw).toBe(yaw);
 
     // No longer poking.
     bugForceTransformUpdatesMock.pokeAll();
     expect(obj.getPosition().x).toBe(x);
+    expect(obj.getRotation().yaw).toBe(yaw);
     bugForceTransformUpdatesMock.pokeAll();
     expect(obj.getPosition().x).toBe(x);
+    expect(obj.getRotation().yaw).toBe(yaw);
     bugForceTransformUpdatesMock.pokeAll();
     expect(obj.getPosition().x).toBe(x);
+    expect(obj.getRotation().yaw).toBe(yaw);
 });
