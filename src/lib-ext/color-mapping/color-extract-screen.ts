e3fecb978a888123963366b31c8315ad391aafc1
darrell <darrell.anderson@gmail.com>
2024-08-20 20:49:58 -0700

    rotating colors, extract colors from rotating color layout

--- src/lib-ext/color-mapping/color-extract-screen.ts
+++ src/lib-ext/color-mapping/color-extract-screen.ts
@@ -1,36 +1,39 @@
 // https://apple.stackexchange.com/questions/87801/how-to-get-color-of-pixel-in-coordinates-123-456-in-screen
 
-import { exec } from "child_process";
+import { execSync } from "child_process";
+import Jimp from "jimp";
 
-const x = 1;
-const y = 1;
+// MacBook Air: 2560 x 1664 Retina
+const x = 1200;
+const y = 100;
+const w = 100;
+const h = 300;
 
-/**
- * @author Chris Vasselli
- *
- * Output is the color in hex, for example, ffffff for white.
- *
- * The screencapture command takes a 1x1 screenshot of the coordinates, and
- * outputs it in the bmp format to a temporary location.
- *
- * The xxd command reads 3 bytes from that file at offset 54. 54 is the length
- * of the header in the generated bmp file, and the next 3 bytes are the color
- * of the pixel.
- *
- * The sed command swaps the first two characters of the output with the last
- * two. This is because the bmp stores the colors in reverse of the normal
- * order - BBGGRR. This command swaps the order to the normal RRGGBB.
- */
-exec(
-    `screencapture -R${x},${y},1,1 -t bmp $TMPDIR/test.bmp && \
-                 xxd -p -l 3 -s 54 $TMPDIR/test.bmp | \
-                 sed 's/\\(..\\)\\(..\\)\\(..\\)/\\3\\2\\1/'`,
-    (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-    }
-);
+async function process() {
+    execSync(`screencapture -R${x},${y},${w},${h} -t png /tmp/px.png`);
+
+    const image = await Jimp.read("/tmp/px.png");
+    const slotColor = Jimp.intToRGBA(image.getPixelColor(174, 101));
+    const objColor = Jimp.intToRGBA(image.getPixelColor(132, 352));
+    const widgetColor = Jimp.intToRGBA(image.getPixelColor(152, 536));
+
+    // Table color at widget position: 173 145 101
+
+    console.log(
+        [
+            "slot:",
+            slotColor.r,
+            slotColor.g,
+            slotColor.b,
+            "obj:",
+            objColor.r,
+            objColor.g,
+            objColor.b,
+            "widget:",
+            widgetColor.r,
+            widgetColor.g,
+            widgetColor.b,
+        ].join(" ")
+    );
+}
+process();
