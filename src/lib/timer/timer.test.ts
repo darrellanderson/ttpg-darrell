770493bb9b0b2eb0ae5b2289b304dd24af4e7651
darrell <darrell.anderson@gmail.com>
2024-09-22 20:44:25 -0700

    timer

---
+++ src/lib/timer/timer.test.ts
@@ -0,0 +1,21 @@
+import { Timer } from "./timer";
+
+it("static getTimeString", () => {
+    expect(Timer.getTimeString(0.1)).toBe("00:00:00");
+    expect(Timer.getTimeString(1)).toBe("00:00:01");
+    expect(Timer.getTimeString(60)).toBe("00:01:00");
+    expect(Timer.getTimeString(3600)).toBe("01:00:00");
+    expect(Timer.getTimeString(3661.1)).toBe("01:01:01");
+});
+
+it("constructor", () => {
+    new Timer("@timer/test");
+});
+
+it("start/stop", () => {
+    const timer = new Timer("@timer/test");
+    timer.setCountdownFromSeconds(100);
+    timer.start();
+    expect(timer.getSeconds()).toBe(100);
+    timer.stop();
+});
---
+++ src/lib/timer/timer.testp.ts
@@ -0,0 +1,22 @@
+import { Button, UIElement, Vector, world } from "@tabletop-playground/api";
+import { Timer } from "./timer";
+
+console.log("Timer test");
+
+const timer: Timer = new Timer("@timer/test").setCountdownFromSeconds(100);
+
+const button: Button = new Button().setFontSize(40).setText("x");
+button.onClicked.add(() => {
+    timer.toggle();
+});
+
+const ui: UIElement = new UIElement();
+ui.position = new Vector(0, 0, world.getTableHeight() + 1);
+ui.widget = button;
+
+world.addUI(ui);
+
+setInterval(() => {
+    console.log(timer.getSecondsFromAnchorTimestamp());
+    button.setText(timer.getTimeString());
+}, 1000);
