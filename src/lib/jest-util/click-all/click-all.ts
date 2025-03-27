c272592acd105300c6e6ff453ea27196af74eac6
darrell <darrell.anderson@gmail.com>
2025-03-26 19:43:14 -0700

    add checkbox to clickAll

--- src/lib/jest-util/click-all/click-all.ts
+++ src/lib/jest-util/click-all/click-all.ts
@@ -1,6 +1,7 @@
 import {
     Border,
     Canvas,
+    CheckBox,
     ContentButton,
     ImageButton,
     LayoutBox,
@@ -11,6 +12,7 @@ import {
 import {
     Button,
     MockButton,
+    MockCheckBox,
     MockContentButton,
     MockImageButton,
     MockPlayer,
@@ -18,26 +20,29 @@ import {
 
 export function clickAll(widget: Widget | undefined, player?: Player) {
     player = player ?? new MockPlayer({ slot: 19 });
-    if (widget instanceof Panel) {
-        for (const child of widget.getAllChildren()) {
-            clickAll(child, player);
-        }
-    } else if (widget instanceof Canvas) {
-        for (const child of widget.getChildren()) {
-            clickAll(child, player);
-        }
-    } else if (widget instanceof Border) {
-        clickAll(widget.getChild(), player);
-    } else if (widget instanceof LayoutBox) {
+    if (widget instanceof Border) {
         clickAll(widget.getChild(), player);
     } else if (widget instanceof Button) {
         const mockButton = widget as MockButton;
         mockButton._clickAsPlayer(player);
+    } else if (widget instanceof Canvas) {
+        for (const child of widget.getChildren()) {
+            clickAll(child, player);
+        }
+    } else if (widget instanceof CheckBox) {
+        const mockCheckBox = widget as MockCheckBox;
+        mockCheckBox._clickAsPlayer(player);
     } else if (widget instanceof ContentButton) {
         const mockButton = widget as MockContentButton;
         mockButton._clickAsPlayer(player);
     } else if (widget instanceof ImageButton) {
         const mockButton = widget as MockImageButton;
         mockButton._clickAsPlayer(player);
+    } else if (widget instanceof LayoutBox) {
+        clickAll(widget.getChild(), player);
+    } else if (widget instanceof Panel) {
+        for (const child of widget.getAllChildren()) {
+            clickAll(child, player);
+        }
     }
 }
